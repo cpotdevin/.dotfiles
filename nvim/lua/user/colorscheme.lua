b3f0946e9afc68827d741318939c693286a2d496
Christian Potdevin <chrpotdevin@gmail.com>
2023-05-20 17:09:59 -0400

    Reset nvim config

--- nvim/lua/user/colorscheme.lua
+++
@@ -1,9 +0,0 @@
-local colorscheme = "gruvbox"
-
-vim.g.gruvbox_contrast_dark = 'hard'
-vim.cmd("autocmd VimEnter * hi Normal ctermbg=none")
-
-local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
-if not status_ok then
-  return
-end
