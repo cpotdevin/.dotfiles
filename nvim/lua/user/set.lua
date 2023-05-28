1ee7e0e1c3de5192730ae97d8ab34c0e8d9816e9
Christian Potdevin <chrpotdevin@gmail.com>
2023-05-28 12:43:12 -0400

    Fix netrw settings

--- nvim/lua/user/set.lua
+++ nvim/lua/user/set.lua
@@ -4,7 +4,7 @@ vim.opt.guicursor = ""
 
 vim.opt.nu = true
 vim.opt.relativenumber = true
-vim.g.netrw_bufsettings = "nu relativenumber"
+vim.g.netrw_bufsettings = "noma nomod nu nobl nowrap ro rnu"
 
 vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
