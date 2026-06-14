35cc7be12e5e5cc5c2c5b5a04dc84ca41ca177d1
Christian Potdevin <chrpotdevin@gmail.com>
2026-06-14 13:33:23 -0400

    Show hiden files in nvim explorer

--- nvim/lua/plugins/oil.lua
+++ nvim/lua/plugins/oil.lua
@@ -3,7 +3,11 @@ return {
     'stevearc/oil.nvim',
     ---@module 'oil'
     ---@type oil.SetupOpts
-    opts = {},
+    opts = {
+      view_options = {
+        show_hidden = true,
+      },
+    },
     -- Optional dependencies
     dependencies = { { "nvim-mini/mini.icons", opts = {} } },
     -- Lazy loading is not recommended because it is very tricky to make it work correctly in all situations.
